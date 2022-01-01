294fc2289abff1a3c9615b2eb81697e086dcf26d
joey493 <ebrahimnader321@gmail.com>
2022-01-01 23:33:59 +0200

    finish homepage

---
+++ src/components/menu-item.jsx
@@ -0,0 +1,13 @@
+const MenuItem = ({ title, img, size }) => {
+    return (
+        <div className={`menu-item ${size}`}>
+            <div className="background-img" style={{ backgroundImage: `url(${img})` }}></div>
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">Shop now</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,9 @@
+import Directory from '../components/directory'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default HomePage
