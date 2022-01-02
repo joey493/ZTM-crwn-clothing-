6de7e3483693b8d6eae7460cc7072d1e603d50d9
joey493 <ebrahimnader321@gmail.com>
2022-01-02 20:35:18 +0200

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+      </Routes>
+
     </div>
   );
 }
